cf7777031ff9bf7ee8c1852b024e578b1e4de1da
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-10 08:21:51 -0500

    refactor components

---
+++ src/components/Definitions.svelte
@@ -0,0 +1,8 @@
+<div class="flex flex-col gap-3 mb-8">
+  <p class="text-gray-300 text-lg selection:bg-indigo-500">
+    Web scraping is the process of using bots to extract content and data from a website.
+  </p>
+  <p class="text-gray-300 text-lg selection:bg-indigo-500">
+    Honestly, I don't really understand Bitwise operators; hopefully this MDN link will help you out
+  </p>
+</div>
---
+++ src/components/Links.svelte
@@ -0,0 +1,23 @@
+<div class="flex flex-col gap-4">
+  <a
+    href="https://google.com"
+    target="_blank"
+    rel="noopener noreferer"
+    class="block bg-indigo-500 py-2 w-full text-center text-white rounded-xl"
+    >Here there are some links</a
+  >
+  <a
+    href="https://google.com"
+    target="_blank"
+    rel="noopener noreferer"
+    class="block bg-indigo-500 py-2 w-full text-center text-white rounded-xl"
+    >Here there are some links</a
+  >
+  <a
+    href="https://google.com"
+    target="_blank"
+    rel="noopener noreferer"
+    class="block bg-indigo-500 py-2 w-full text-center text-white rounded-xl"
+    >Here there are some links</a
+  >
+</div>
