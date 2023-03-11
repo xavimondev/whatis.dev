7b52c427f7bf66d484d3dd35f5983273bcf67ebf
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-11 17:39:05 -0500

    add icons

---
+++ src/components/icons/ScreenIc.svelte
@@ -0,0 +1,19 @@
+<svg
+  class="h-4 w-4 sm:h-6 sm:w-6"
+  stroke-width="1.8"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  color="#000000"
+  ><path
+    d="M3 17V7a2 2 0 012-2h14a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+    stroke="#000000"
+    stroke-width="1.8"
+  /><path
+    d="M6 8h1"
+    stroke="#000000"
+    stroke-width="1.8"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  /></svg
+>
---
+++ src/components/icons/VideoIc.svelte
@@ -0,0 +1,21 @@
+<svg
+  class="h-4 w-4 sm:h-6 sm:w-6"
+  stroke-width="1.8"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  color="#000000"
+  ><path
+    d="M21 3.6v16.8a.6.6 0 01-.6.6H3.6a.6.6 0 01-.6-.6V3.6a.6.6 0 01.6-.6h16.8a.6.6 0 01.6.6z"
+    stroke="#000000"
+    stroke-width="1.8"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  /><path
+    d="M9.898 8.513a.6.6 0 00-.898.52v5.933a.6.6 0 00.898.521l5.19-2.966a.6.6 0 000-1.042l-5.19-2.966z"
+    stroke="#000000"
+    stroke-width="1.8"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  /></svg
+>
