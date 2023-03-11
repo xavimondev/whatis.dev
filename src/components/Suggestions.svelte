0fcc19d662fce43eae2bc5c44b974b481f1cd099
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-11 17:50:48 -0500

    add suggestions component

---
+++ src/components/Suggestions.svelte
@@ -0,0 +1,28 @@
+<div class="mt-2 sm:max-w-lg">
+  <ul class="flex gap-3 flex-wrap">
+    <li>
+      <button
+        class="bg-indigo-600 rounded-xl text-white font-semibold p-2.5 hover:bg-indigo-700 transition hover:scale-110"
+        >PR</button
+      >
+    </li>
+    <li>
+      <button
+        class="bg-indigo-600 rounded-xl text-white font-semibold p-2.5 hover:bg-indigo-700 transition hover:scale-110"
+        >Typo</button
+      >
+    </li>
+    <li>
+      <button
+        class="bg-indigo-600 rounded-xl text-white font-semibold p-2.5 hover:bg-indigo-700 transition hover:scale-110"
+        >IDE</button
+      >
+    </li>
+    <li>
+      <button
+        class="bg-indigo-600 rounded-xl text-white font-semibold p-2.5 hover:bg-indigo-700 transition hover:scale-110"
+        >CMS</button
+      >
+    </li>
+  </ul>
+</div>
