356d10b1edf85497d87996fc61ad00f3dabb12f1
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-11 08:40:07 -0500

    add data is displayed on components

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -1,23 +1,15 @@
+<script>
+  import { termDataStore } from '../state/store.js'
+</script>
+
 <div class="flex flex-col gap-4">
-  <a
-    href="https://google.com"
-    target="_blank"
-    rel="noopener noreferer"
-    class="block bg-indigo-500 py-2 w-full text-center text-white rounded-xl"
-    >Here there are some links</a
-  >
-  <a
-    href="https://google.com"
-    target="_blank"
-    rel="noopener noreferer"
-    class="block bg-indigo-500 py-2 w-full text-center text-white rounded-xl"
-    >Here there are some links</a
-  >
-  <a
-    href="https://google.com"
-    target="_blank"
-    rel="noopener noreferer"
-    class="block bg-indigo-500 py-2 w-full text-center text-white rounded-xl"
-    >Here there are some links</a
-  >
+  {#each $termDataStore.links as link}
+    <a
+      href={link.resource}
+      target="_blank"
+      rel="noopener noreferer"
+      class="block bg-indigo-500 py-2.5 w-full text-center text-white rounded-xl"
+      >Read more about this on <span class="text-indigo-900 font-semibold">{link.name}</span></a
+    >
+  {/each}
 </div>
--- src/components/Results.svelte
+++ src/components/Results.svelte
@@ -1,17 +1,18 @@
 <script>
   import Definitions from './Definitions.svelte'
   import Links from './Links.svelte'
+  import { termDataStore } from '../state/store.js'
 </script>
 
-<section class="flex flex-col max-w-xl">
-  <h2 class="text-white font-semibold text-2xl mb-14 text-center">
-    What is <span class="text-indigo-600"> Web Scrapping</span> ?
-  </h2>
-  <!-- Definitions -->
-  <Definitions />
-  <!-- Interesting links -->
-  <div class="flex flex-col gap-4 mb-5">
-    <h3 class="text-white text-xl">Some links to dive deeper into</h3>
-    <Links />
-  </div>
-</section>
+{#if $termDataStore}
+  <section class="flex flex-col max-w-xl">
+    <h2 class="text-white font-semibold text-2xl mb-8 text-center">
+      What is <span class="text-indigo-600"> {$termDataStore.name}</span> ?
+    </h2>
+    <Definitions />
+    <div class="flex flex-col gap-4 mb-8">
+      <h3 class="text-white text-xl">Some links to dive deeper into</h3>
+      <Links />
+    </div>
+  </section>
+{/if}
