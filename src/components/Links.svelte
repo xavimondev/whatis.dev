8df0308ec971dcf4a59f2c4c740240e173ead0e9
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-11 17:47:32 -0500

    add icon is displayes based on resource's type

--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -1,15 +1,23 @@
 <script>
+  import VideoIc from '../components/icons/VideoIc.svelte'
+  import ScreenIc from '../components/icons/ScreenIc.svelte'
   import { termDataStore } from '../state/store.js'
 </script>
 
 <div class="flex flex-col gap-4">
-  {#each $termDataStore.links as link}
+  {#each $termDataStore.links as { resource, type, name }}
     <a
-      href={link.resource}
+      href={resource}
       target="_blank"
       rel="noopener noreferer"
-      class="block bg-indigo-500 py-2.5 w-full text-center text-white rounded-xl"
-      >Read more about this on <span class="text-indigo-900 font-semibold">{link.name}</span></a
+      class="flex bg-indigo-500 py-2.5 px-2 w-full text-white rounded-xl items-center gap-2"
+    >
+      {#if type === 'doc'}
+        <ScreenIc />
+      {:else}
+        <VideoIc />
+      {/if}
+      Learn more about this on <span class="text-indigo-900 font-semibold">{name}</span></a
     >
   {/each}
 </div>
