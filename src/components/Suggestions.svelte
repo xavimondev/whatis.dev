b1ec20e18ba1f3dcad1d711b073955ea63182400
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-13 22:15:17 -0500

    add results are displayed when user clicks on suggestion

--- src/components/Suggestions.svelte
+++ src/components/Suggestions.svelte
@@ -1,28 +1,30 @@
+<script>
+  import { termDataStore, inputStore } from '../state/store.js'
+  import { searchMeaning } from '../utils/searchMeaning'
+  import { updateQueryParams, removeQueryParams } from '../utils/queryParams'
+
+  const SUGGESTIONS = ['PR', 'DevOps', 'IDE', 'UX', 'CI', 'Scrapping']
+
+  const searchBySuggestion = (suggestion) => {
+    const termData = searchMeaning(suggestion)
+    termDataStore.set(termData)
+    if (termData) updateQueryParams(suggestion)
+    else {
+      inputStore.set(suggestion)
+      removeQueryParams('q')
+    }
+  }
+</script>
+
 <div class="mt-2 sm:max-w-lg">
   <ul class="flex gap-3 flex-wrap">
-    <li>
-      <button
-        class="bg-indigo-600 rounded-xl text-white font-semibold p-2.5 hover:bg-indigo-700 transition hover:scale-110"
-        >PR</button
-      >
-    </li>
-    <li>
-      <button
-        class="bg-indigo-600 rounded-xl text-white font-semibold p-2.5 hover:bg-indigo-700 transition hover:scale-110"
-        >Typo</button
-      >
-    </li>
-    <li>
-      <button
-        class="bg-indigo-600 rounded-xl text-white font-semibold p-2.5 hover:bg-indigo-700 transition hover:scale-110"
-        >IDE</button
-      >
-    </li>
-    <li>
-      <button
-        class="bg-indigo-600 rounded-xl text-white font-semibold p-2.5 hover:bg-indigo-700 transition hover:scale-110"
-        >CMS</button
-      >
-    </li>
+    {#each SUGGESTIONS as suggestion}
+      <li>
+        <button
+          class="bg-indigo-600 rounded-xl text-white font-semibold p-2.5 hover:bg-indigo-700 transition hover:scale-110"
+          on:click={() => searchBySuggestion(suggestion)}>{suggestion}</button
+        >
+      </li>
+    {/each}
   </ul>
 </div>
