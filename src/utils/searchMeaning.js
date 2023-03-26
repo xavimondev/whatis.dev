34d51cb52c25856ad311af3c157357773f28b983
Xavier Alfaro <nxb.developer@gmail.com>
2023-03-25 21:32:23 -0500

    update util to get meaning according to lang

--- src/utils/searchMeaning.js
+++ src/utils/searchMeaning.js
@@ -1,7 +1,15 @@
-import data from '../data/data.json'
+import { getLangFromUrl } from '../i18n/utils'
+import enData from '../i18n/data/en.json'
+import esData from '../i18n/data/es.json'
+
+const data = {
+  en: enData,
+  es: esData
+}
 
 export const searchMeaning = (termValue) => {
-  const listOfTerms = data.data
+  const lang = getLangFromUrl(new URL(window.location.href))
+  const listOfTerms = data[lang].data
   return listOfTerms.find((term) =>
     term.name.toLowerCase() === termValue.toLowerCase()
   )
