b5ad2402dd4803dcb9c2a0312885995cfdb4b85c
Xavier Alfaro <nxb.developer@gmail.com>
2023-04-01 23:38:53 -0500

    add util to show options as user is searching

--- src/utils/searchMeaning.js
+++ src/utils/searchMeaning.js
@@ -1,3 +1,4 @@
+import Fuse from 'fuse.js'
 import { getLangFromUrl } from '../i18n/utils'
 import enData from '../i18n/data/en.json'
 import esData from '../i18n/data/es.json'
@@ -5,6 +6,19 @@ import esData from '../i18n/data/es.json'
 const data = {
   en: enData,
   es: esData
+}
+
+export const listOptionsByTerm = (query) => {
+  const lang = getLangFromUrl(new URL(window.location.href))
+  const listOfTerms = data[lang].data
+  const fuse = new Fuse(listOfTerms, {
+    keys: ['name'],
+    isCaseSensitive: false,
+    includeMatches: true,
+    findAllMatches: true,
+    minMatchCharLength: 2
+  })
+  return fuse.search(query)
 }
 
 export const searchMeaning = (termValue) => {
