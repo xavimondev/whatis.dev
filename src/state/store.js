4413239b1c02b120b1c2553164f3c37d729e381d
Xavier Alfaro <nxb.developer@gmail.com>
2023-04-02 09:07:51 -0500

    add comments

--- src/state/store.js
+++ src/state/store.js
@@ -1,6 +1,10 @@
 import { writable } from 'svelte/store'
 
+// Load option list as user is typing on input search
 export const termListOptions = writable([])
+// Used to show wrong term that does not exist
 export const inputStore = writable('')
+// Used to show input's value on field
 export const inputValue = writable('')
+// Term selected by user
 export const termSelected = writable(undefined)
