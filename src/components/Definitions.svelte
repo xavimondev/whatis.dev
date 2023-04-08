27f8186adeec34450d68bb5914cc03d25addd9fc
Xavier Alfaro <nxb.developer@gmail.com>
2023-04-08 12:08:35 -0500

    fix user can not look for other new term

--- src/components/Definitions.svelte
+++ src/components/Definitions.svelte
@@ -3,9 +3,11 @@
 </script>
 
 <div class="flex flex-col gap-3 mb-8">
-  {#each $termSelected.meanings as meaning}
-    <p class="text-gray-300 text-lg selection:bg-indigo-500">
-      {meaning.description}
-    </p>
-  {/each}
+  {#if $termSelected}
+    {#each $termSelected.meanings as meaning}
+      <p class="text-gray-300 text-lg selection:bg-indigo-500">
+        {meaning.description}
+      </p>
+    {/each}
+  {/if}
 </div>
--- src/components/Links.svelte
+++ src/components/Links.svelte
@@ -9,19 +9,21 @@
 </script>
 
 <div class="flex flex-col gap-4">
-  {#each $termSelected.links as { resource, type, name }}
-    <a
-      href={resource}
-      target="_blank"
-      rel="noopener noreferrer"
-      class="flex bg-indigo-500 py-2.5 px-2 w-full text-white rounded-xl items-center gap-2"
-    >
-      {#if type === 'doc'}
-        <ScreenIc />
-      {:else}
-        <VideoIc />
-      {/if}
-      {t('link.text')} <span class="text-indigo-900 font-semibold">{name}</span></a
-    >
-  {/each}
+  {#if $termSelected}
+    {#each $termSelected.links as { resource, type, name }}
+      <a
+        href={resource}
+        target="_blank"
+        rel="noopener noreferrer"
+        class="flex bg-indigo-500 py-2.5 px-2 w-full text-white rounded-xl items-center gap-2"
+      >
+        {#if type === 'doc'}
+          <ScreenIc />
+        {:else}
+          <VideoIc />
+        {/if}
+        {t('link.text')} <span class="text-indigo-900 font-semibold">{name}</span></a
+      >
+    {/each}
+  {/if}
 </div>
