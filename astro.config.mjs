8f2c9313524ad455c4ae298aca124be73587cf8b
Xavier Alfaro <nxb.developer@gmail.com>
2023-04-08 17:42:52 -0500

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import svelte from '@astrojs/svelte'
+import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  adapter: vercel({
+    analytics: true
+  })
 })
