1172cf0773edca9c3f2c51478e9699319b3a9f62
Xavier Alfaro <nxb.developer@gmail.com>
2023-04-18 12:37:19 -0500

    update svg color

--- src/components/icons/ScreenIc.svelte
+++ src/components/icons/ScreenIc.svelte
@@ -4,14 +4,14 @@
   viewBox="0 0 24 24"
   fill="none"
   xmlns="http://www.w3.org/2000/svg"
-  color="#000000"
+  color="#ffffff"
   ><path
     d="M3 17V7a2 2 0 012-2h14a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2z"
-    stroke="#000000"
+    stroke="#ffffff"
     stroke-width="1.8"
   /><path
     d="M6 8h1"
-    stroke="#000000"
+    stroke="#ffffff"
     stroke-width="1.8"
     stroke-linecap="round"
     stroke-linejoin="round"
